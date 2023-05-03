cc87cebc2f85189a8766ed53b5f14596600d8726
Roveri <gabriel.roveri7@gmail.com>
2023-05-03 13:43:41 -0300

    Initial Commit

---
+++ SolutisSW2023/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SolutisSW2023
+//
+//  Created by Gabriel Roveri on 03/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
